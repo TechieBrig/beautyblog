70ac666a382f8f191087f787226fcace586aabf4
Brigette Hinton <brigette.hinton@hotmail.com>
2016-04-08 18:25:03 -0500

    Updating JS

---
+++ beautyblog.js
@@ -0,0 +1,55 @@
+Blogs = new Mongo.Collection("blogs")
+
+Meteor.methods({
+  createPost: function  (text) {
+  
+
+    Blogs.insert({
+      title: title,
+      body: body,
+      createdAt: new Date(),
+    
+  });
+},
+
+deletePost: function(post)
+
+Posts.remove(postId);
+}
+});
+
+Template.body.helpers({
+  posts: function() {
+    return Posts.find({}, {
+      sort: { createdAt: -1 }
+    });
+  }
+});
+
+Template.body.events({
+  'submit .new-post': function (event) {
+    event.preventDefault();
+
+  Meteor.call('createPost', event.target.text)
+
+    event.target.text.value ='';
+  }
+});
+
+  Template.post.helpers({
+    time: function() {
+      return moment(this.createdAt).fromNow();
+    },
+});
+
+  Template.post.events({
+    'click .delete-post': function(event) {
+      Meteor.call('deletePost');
+    }
+  });
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+  });
+}
+
